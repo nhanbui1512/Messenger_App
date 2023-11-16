26ac0bde5491440b699540fdff668bd0cb83e73a
nhanbui1512 <buithiennhan0345@gmail.com>
2023-11-17 00:20:47 +0700

    css hidden for actions wrapper

--- src/Components/ChatBox/MessageItem/Actions.js
+++ src/Components/ChatBox/MessageItem/Actions.js
@@ -2,18 +2,21 @@ import classNames from 'classnames/bind';
 import styles from './MessageItem.module.scss';
 import CircleButton from '../../CircleButton';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faEllipsisVertical, faFaceSmile } from '@fortawesome/free-solid-svg-icons';
-import { Upload } from '../../Icons';
+import { faEllipsisVertical, faFaceSmile, faReply } from '@fortawesome/free-solid-svg-icons';
 const cx = classNames.bind(styles);
 
 export default function Actions() {
   return (
     <div className={cx('action_wrapper')}>
-      <CircleButton transparent className={cx('action-btn')} icon={<Upload />} />
       <CircleButton
         transparent
         className={cx('action-btn')}
         icon={<FontAwesomeIcon icon={faFaceSmile} />}
+      />
+      <CircleButton
+        transparent
+        className={cx('action-btn')}
+        icon={<FontAwesomeIcon icon={faReply} />}
       />
       <CircleButton
         transparent
