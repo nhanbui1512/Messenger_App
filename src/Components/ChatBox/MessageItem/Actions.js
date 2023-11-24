0bb45a3087df87b4acff4df91f17f11223343834
nhanbui1512 <buithiennhan0345@gmail.com>
2023-11-24 22:10:10 +0700

    build UI emotion btns

--- src/Components/ChatBox/MessageItem/Actions.js
+++ src/Components/ChatBox/MessageItem/Actions.js
@@ -3,22 +3,53 @@ import styles from './MessageItem.module.scss';
 import CircleButton from '../../CircleButton';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faEllipsisVertical, faFaceSmile, faReply } from '@fortawesome/free-solid-svg-icons';
+import reactsImage from '../../../assests/images/reactions';
 
 import Tippy from '@tippyjs/react';
 import 'tippy.js/dist/tippy.css';
+import Image from '../../Image';
 
 const cx = classNames.bind(styles);
 
-export default function Actions() {
+export default function Actions({ emotions, setEmotions }) {
   return (
     <div className={cx('action_wrapper')}>
-      <Tippy content="Bày tỏ cảm xúc">
-        <div>
+      <Tippy zIndex={98} content="Bày tỏ cảm xúc">
+        <div className="relative">
           <CircleButton
+            onClick={() => {
+              setEmotions(!emotions);
+            }}
             transparent
             className={cx('action-btn')}
             icon={<FontAwesomeIcon icon={faFaceSmile} />}
           />
+
+          {emotions && (
+            <div className={cx('emotion-wrapper')}>
+              <div className={cx('emotion-btn')}>
+                <Image src={reactsImage.heart} />
+              </div>
+
+              <div className={cx('emotion-btn')}>
+                <Image src={reactsImage.laugh} />
+              </div>
+
+              <div className={cx('emotion-btn', { active: true })}>
+                <Image src={reactsImage.wow} />
+              </div>
+
+              <div className={cx('emotion-btn')}>
+                <Image src={reactsImage.sad} />
+              </div>
+              <div className={cx('emotion-btn')}>
+                <Image src={reactsImage.angry} />
+              </div>
+              <div className={cx('emotion-btn')}>
+                <Image src={reactsImage.like} />
+              </div>
+            </div>
+          )}
         </div>
       </Tippy>
       <Tippy content="Trả lời">
--- src/Components/ChatBox/MessageItem/index.js
+++ src/Components/ChatBox/MessageItem/index.js
@@ -3,21 +3,38 @@ import styles from './MessageItem.module.scss';
 import CircleImage from '../../CircleImage';
 import Actions from './Actions';
 import Reactions from './Reactions';
+import Tippy from '@tippyjs/react/headless';
+import { useState } from 'react';
 
 const cx = classNames.bind(styles);
 
 export default function MessageItem({ myself = false, contents = [] }) {
+  const [emotions, setEmotions] = useState(false);
+
   const renderContent = () => {
     return contents.map((item, index) => {
       const first = index === 0;
       const last = index === contents.length - 1;
+
       return (
         <div key={index} className={cx('mess-wrap')}>
-          <div className={cx('mess-container', { first, last })}>
-            <span className={cx('mess-content')}>{item}</span>
-          </div>
+          <Tippy
+            hideOnClick
+            placement="right"
+            interactive
+            onHide={() => {
+              setEmotions(false);
+            }}
+            appendTo={'parent'}
+            render={(atrs) => {
+              return <Actions emotions={emotions} setEmotions={setEmotions} />;
+            }}
+          >
+            <div className={cx('mess-container', { first, last })}>
+              <span className={cx('mess-content')}>{item}</span>
+            </div>
+          </Tippy>
 
-          <Actions />
           <div>
             <Reactions />
           </div>
